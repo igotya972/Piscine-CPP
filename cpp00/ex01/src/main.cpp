6fe2b5bea3b22e133404debce4fbb94b49f83086
igotya972 <dferjul@c1r3p5.42nice.fr>
2024-08-08 22:20:34 +0200

    add fonctions

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dferjul <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 20:25:33 by dferjul           #+#    #+#             */
-/*   Updated: 2024/08/08 22:01:35 by dferjul          ###   ########.fr       */
+/*   Updated: 2024/08/08 22:16:01 by dferjul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,12 @@ int main ()
 	std::string phone_number;
 	std::string nickname;
 	std::string darkest_secret;
+	std::string last_name;
 	
 	contact.set_first_name(first_name);
 	contact.set_phone_number(phone_number);
 	contact.set_nickname(nickname);
 	contact.set_darkest_secret(darkest_secret);
+	contact.set_last_name(last_name);
 	return (0);
 }
