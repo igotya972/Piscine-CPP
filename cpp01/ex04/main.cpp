f1cc230f5b9d92d4c8614d438a6b56f9919ebf35
igotya972 <damscrypto@gmail.com>
2024-09-04 15:42:20 +0200

    cpp01-ex04 finish

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -15,34 +15,50 @@
 #include <sstream>
 #include <string>
 
+std::string ft_replace(std::string line, std::string s1, std::string s2)
+{
+	std::string result;
+	size_t pos = 0;
+	size_t pos_len = line.find(s1);
+	while (pos_len != std::string::npos)
+	{
+		result += line.substr(pos, pos_len - pos);
+		result += s2;
+		pos = pos_len + s1.length();
+		pos_len = line.find(s1, pos);
+	}
+	result += line.substr(pos);
+	return (result);
+}
+
 int main(int ac, char **av)
 {
 	if (ac != 4)
 	{
-		std::cout << "Usage: ./replace [filename] [string1] [string2]" << std::endl;
-		return 1;
+		std::cout << "Usage: ./replace [file_name] [string1] [string2]" << std::endl;
+		return (1);
 	}
 	std::ifstream file(av[1]);
 	if (!file.is_open())
 	{
 		std::cout << "Error: could not open file" << std::endl;
-		return 1;
+		return (1);
 	}
 	std::string filename = av[1];
 	std::string s1 = av[2];
 	std::string s2 = av[3];
 	std::string line;
-	std::ofstream output((filename + ".replace").c_str());
-	if (!output.is_open())
+	std::ofstream output_file((filename + ".replace").c_str());
+	if (!output_file.is_open())
 	{
-		std::cout << "Error: could not create output file" << std::endl;
-		return 1;
+		std::cout << "Error: could not create file" << std::endl;
+		return (1);
 	}
 	while (std::getline(file, line))
 	{
-		
+		output_file << ft_replace(line, s1, s2) << std::endl;
 	}
 	file.close();
-	output.close();
-	return 0;
+	output_file.close();
+	return (0);
 }
