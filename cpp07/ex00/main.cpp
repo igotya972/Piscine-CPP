659d8d81cc4d9e236831217d06517f8de8326ad9
Dam's42 <110895268+igotya972@users.noreply.github.com>
2024-12-03 23:45:03 +0100

    cpp07-start

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dferjul <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 12:46:27 by dferjul           #+#    #+#             */
+/*   Updated: 2024/12/03 23:15:32 by dferjul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/Base.hpp"
+
+int main ()
+{
+	Base *Welcome = generate();
+	std::cout << "Adresse: " << Welcome << std::endl;
+	std::cout << "Pointer: ";
+	identify(Welcome);
+	std::cout << "Reference: ";
+	identify(*Welcome);
+	delete Welcome;
+	return 0;
+}
